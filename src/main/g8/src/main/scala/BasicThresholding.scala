fc96566ea37ba6a4526d35fc11113789bdea08e5
Nicolas Modrzyk <hellonico@gmail.com>
2020-02-06 11:37:53 +0900

    BasicThresholding.scala

---
+++ src/main/g8/src/main/scala/BasicThresholding.scala
@@ -0,0 +1,19 @@
+import org.opencv.core.Mat
+import origami.Origami
+import org.opencv.imgcodecs.Imgcodecs.{IMREAD_COLOR, imread, imwrite}
+import org.opencv.imgproc.Imgproc.{THRESH_BINARY, THRESH_BINARY_INV, THRESH_TOZERO, THRESH_TOZERO_INV, threshold}
+import origami.Origami.urlToMat
+
+object BasicThresholding extends App {
+    Origami.init()
+    var source = urlToMat(args.apply(0))
+    val destination = new Mat(source.rows, source.cols, source.`type`)
+    threshold(source, destination, 127, 255, THRESH_TOZERO)
+    imwrite("ThreshZero.jpg", destination)
+    threshold(source, destination, 127, 255, THRESH_TOZERO_INV)
+    imwrite("ThreshZeroInv.jpg", destination)
+    threshold(source, destination, 127, 255, THRESH_BINARY)
+    imwrite("ThreshBinary.jpg", destination)
+    threshold(source, destination, 127, 255, THRESH_BINARY_INV)
+    imwrite("ThreshBinaryInv.jpg", destination)
+}
